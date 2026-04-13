33b295ba9ae178305cb69b2cf6c34565c65c7227
Bruno/banish417.me <bruninvest100@gmail.com>
2026-04-12 21:01:38 -0300

    Atualiza arquivos modificados e adiciona render.yaml

--- app/database.py
+++ app/database.py
@@ -1,12 +1,17 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./tasks.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./tasks.db")
+
+connect_args = {} 
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL, connect_args=connect_args
 )
 
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 Base = declarative_base()
